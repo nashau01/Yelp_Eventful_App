4fe432b417b4585757a166471f80c7768922d76e
nashau01 <nashau01@luther.edu>
2016-04-05 13:10:13 -0500

    Preliminaries
    API homepages, client libraries, application keys

---
+++ mt_mashup.py
@@ -0,0 +1,17 @@
+# Eventful API
+# homepage: http://api.eventful.com
+# python client library: http://api.eventful.com/libs/python/
+# application key: hLdVs3LKGBLbjMfd
+
+# Yelp API
+# homepage: https://www.yelp.com/developers
+# python client library: https://github.com/Yelp/yelp-python
+# Keys:
+"""
+{
+    "consumer_key": "mvC1SY7wAUx_RPlVhG-fIw",
+    "consumer_secret": "vkqWVoowxsWkUu7KU0t2Pj3qY1k",
+    "token": "syYEIdwvGt-uLGdgVmu9ZsQfE98CROe4",
+    "token_secret": "AQLQnKJA7VYw4XlVIMK7hYzSwDo"
+}
+"""
