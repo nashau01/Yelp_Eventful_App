35828302c18011810e61bf8883c5163a6a649404
Kaleb V <vangka02@gmail.com>
2016-04-06 10:26:50 -0500

    comments added

--- mt_mashup.py
+++ mt_mashup.py
@@ -6,13 +6,7 @@
 # Yelp API
 # homepage: https://www.yelp.com/developers
 # python client library: https://github.com/Yelp/yelp-python
-# Keys:
 
-
-
-
-
-#import yelp
 import eventful
 import yelp
 from yelp.client import Client
@@ -21,32 +15,33 @@ import io
 import json
 
 
-# read API keys
-with io.open('config_secret.json') as cred:
+#activate eventful API with key
+api = eventful.API('hLdVs3LKGBLbjMfd')
+
+# read API keys for yelp
+with io.open('config_secret.json') as cred:       #authentication keys are stored in secret json file which is ignored in the .gitignore file
     creds = json.load(cred)
     auth = Oauth1Authenticator(**creds)
     client = Client(auth)
 
-
 client = Client(auth)
 
-
-api = eventful.API('hLdVs3LKGBLbjMfd')
 
 # If you need to log in:
 # api.login('username', 'password')
 
-events = api.call('/events/search', q='concert', l='Decorah')
+
+#use of eventful API
+events = api.call('/events/search', q='concert', l='Decorah')    #search eventful via API
 for event in events['events']['event']:
     print (event['title'], "," , event['venue_name'], ",", event['city_name'], ",", event['start_time'])
 
 
+#use of yelp API
 params = {
     'term': 'food',
     'lang': 'en'
 }
 
-response = client.search('Decorah', **params)
-
-
-print(response.businesses[1].name)
+response = client.search('Decorah', **params)      #pass the params
+print(response.businesses[0].name)
