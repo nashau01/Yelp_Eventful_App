123418287b5c8552de7a4d976766163ff1f3830f
austinnash <nashau01@luther.edu>
2016-04-07 10:14:18 -0500

    Started writing python classes

--- mt_mashup.py
+++ mt_mashup.py
@@ -46,3 +46,31 @@ params = {
 
 response = client.search('Decorah', **params)      #pass the params
 print(response.businesses[0].name)
+
+
+
+class ScheduleOption:
+    def __init__(self, yelp_results, eventful_results):
+        yelp_results = yelp_results
+        eventful_results = eventful_results
+        score = 0
+
+    def calculateScore(self):
+        #find the score (weight)
+
+class ScheduleMaker:
+    def __init__(self, yelp_results, eventful_results):
+        yelp_results = yelp_results
+        eventful_results = eventful_results
+
+    def findPossibleSchedules(self, numOptions):
+        options_list = []
+        for i in range(numOptions):
+            options_list.append(findNewOption())
+
+        return options_list
+
+    def findNewOption(self):
+        #find a new option using the results
+        option = ScheduleOption(yelp_results, eventful_results)
+        return option
