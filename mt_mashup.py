00ff4525c55b921359d43a25c0e28ce78dd2c72e
Kaleb V <vangka02@gmail.com>
2016-04-16 09:57:48 -0500

    Migrated Yelp API Calls to module; Implemented several dictionary components including full maps functionality

--- mt_mashup.py
+++ mt_mashup.py
@@ -23,16 +23,6 @@ from flask import Flask, request, render_template, redirect
 
 ##### Called Functions #####
 
-def search_yelp():
-    #with io.open('config_secret.json') as cred:       #authentication keys are stored in secret json file which is ignored in the .gitignore file
-        #creds = json.load(cred)
-    auth = Oauth1Authenticator(consumer_key = "mvC1SY7wAUx_RPlVhG-fIw",consumer_secret = "vkqWVoowxsWkUu7KU0t2Pj3qY1k",token = "syYEIdwvGt-uLGdgVmu9ZsQfE98CROe4", token_secret = "AQLQnKJA7VYw4XlVIMK7hYzSwDo")
-                                   #**creds)
-    client = Client(auth)
-
-    results = client.search(parameters["both"]["location_center"], **parameters["yelp"])      #pass the params
-    return results
-
 def search_eventful():
     api = eventful.API('hLdVs3LKGBLbjMfd')        #activate key
 
@@ -44,13 +34,9 @@ def search_eventful():
     return events['events']['event']
 
 def get_results():
-    yelp_results = search_yelp()
     eventful_results = search_eventful()
-
     # parse results with ye_schedule module
-
-    schedule_maker = ScheduleMaker(yelp_results, eventful_results, 5, 4)
-
+    schedule_maker = ScheduleMaker(eventful_results, 5, 4, parameters["yelp"])
     return schedule_maker.options_list
 
 #### Initialize Parameter Dictionaries #####
@@ -77,7 +63,7 @@ parameters["yelp"] = {
     "term" : "", #(keywords, e.g. "food", "restaurants")
     #"sort" : None, #(similar to eventful sort order, e.g. "Best Matched", "Highest Rated")
     #"category_filter" : "", #(internal, based on type of activity/entertainment)
-    "price_range" : None
+    #"price_range" : None
 }
 
 ##### App Control #####
@@ -98,64 +84,12 @@ def reset():
 def my_form_post():
     location = request.form['location']
     parameters["eventful"]["keywords"] = request.form['etype']
-    parameters["yelp"]["keywords"] = request.form['dtype']
-    parameters["both"]["location_radius"] = request.form['distance']
+    parameters["yelp"]["term"] = request.form['dtype']
     parameters["both"]["date"] = request.form['date']
     #parameters["eventful"]["price_range"] = request.form['eprice']
     #parameters["yelp"]["price_range"] = request.form['dprice']
     parameters["both"]["location_center"] = request.form['location']
     
-    
-    """
-    sample_dict = {
-            'event' : 'Baseball Game',
-            'dining' : [{'name' : 'Waffle House',
-                            'address' : '106 North Ave., Minneapolis, MN',
-                            'price' : '$',
-                            'lat' : 44.968046,
-                            'lng' : -94.420307
-                        },
-                        {'name' : 'Perkin\'s',
-                        'address' :'3000 Busy Rd., Minneapolis, MN',
-                        'price' : '$$',
-                        'lat' : 44.33328,
-                        'lng' : -89.132008}, {'name' : 'IHOP', 'address' : '720 Long St., Minneapolis, MN', 'price' : '$$', 'lat' : 33.755787 , 'lng' : -116.359998}, {'name' : 'Denny\'s', 'address' : '404 Error St., Minneapolis, MN', 'price' : '$$', 'lat' : 33.844843 , 'lng' : -116.54911}],
-            'date' : '2016-04-15',
-            'lat' : 44.92057,
-            'lng' : -93.44786,
-            'time' : '6:00pm',
-            'cost' : '$$$',
-            'venue' : 'Baseball Field',
-            'city' : 'Minneapolis, MN',
-            'description' : "Baseball is a bat-and-ball game played between two teams of nine players each who take turns batting and fielding.The batting team attempts to score runs by hitting a ball that is thrown by the pitcher with a bat swung by the batter, then running counter-clockwise around a series of four bases: first, second, third, and home plate. A run is scored when a player advances around the bases and returns to home plate.Players on the batting team take turns hitting against the pitcher of the fielding team, which tries to prevent runs by getting hitters out in any of several ways. A player on the batting team who reaches a base safely can later attempt to advance to subsequent bases during teammates' turns batting, such as on a hit or by other means. The teams switch between batting and fielding whenever the fielding team records three outs. One turn batting for both teams, beginning with the visiting team, constitutes an inning. A game comprises nine innings, and the team with the greater number of runs at the end of the game wins. Baseball is the only major team sport in America with no game clock, although almost all games end in the ninth inning.Evolving from older bat-and-ball games, an early form of baseball was being played in England by the mid-18th century. This game was brought by immigrants to North America, where the modern version developed. By the late 19th century, baseball was widely recognized as the national sport of the United States. Baseball is now popular in North America and parts of Central and South America, the Caribbean, and East Asia.  In the United States and Canada, professional Major League Baseball (MLB) teams are divided into the National League (NL) and American League (AL), each with three divisions: East, West, and Central. The major league champion is determined by playoffs that culminate in the World Series. The top level of play is similarly split in Japan between the Central and Pacific Leagues and in Cuba between the West League and East League."
-            }
-            
-    possibilities = []
-    possibilities.append(sample_dict)
-
-
-    for i in range(0,19):
-        possibilities.append(sample_dict.copy())
-    
-    count = 1
-    for dict in possibilities:
-        scount = str(count)
-        dict['id'] = "id" + scount
-        dict['mapno'] = "map" + scount
-        dict['mapprop'] = "mp" + scount
-        dict['center'] = "center" + scount
-        dict['info'] = "info" + scount
-        for item in dict['dining']:
-            item['marker'] = "marker" + str(count * 2)
-            item['info'] = "dinfo" + str(count * 2) 
-            count += 1
-        count += 1
-        print(count)
-        print(dict['id'])
-        """
-
-#'cost' : '$$$'
-# }
     possibilities = []
     #possibilities.append(sample_dict)
 
@@ -171,9 +105,11 @@ def my_form_post():
         for a_dining_option in an_option.dining_activities:
             a_dining_dict = {}
             a_dining_dict['name'] = a_dining_option.name
-            a_dining_dict['address'] = "unimplemented"
-            a_dining_dict['lat'] = "unimplemented"
-            a_dining_dict['lng'] = "unimplemented"
+            a_dining_dict['address'] = a_dining_option.address
+            a_dining_dict['phone'] = a_dining_option.phone 
+            a_dining_dict['lat'] = a_dining_option.lat
+            a_dining_dict['lng'] = a_dining_option.lng 
+            a_dining_dict['rating'] = a_dining_option.rating
             a_dict['dining'].append(a_dining_dict)
 
         """ To implement date: assign the date attribute in the Activity constructor
@@ -182,13 +118,13 @@ def my_form_post():
         a_dict['date'] = "unimplemented"
         a_dict['time'] = an_option.event_activity.start_time
         a_dict['cost'] = '?'
-        a_dict['venue'] = "unimplemented"
+        a_dict['venue'] = an_option.event_activity.venue
         a_dict['address'] = "unimplemented"
-        a_dict['description'] = "unimplemented"
+        a_dict['description'] = an_option.event_activity.description
         
         # behind the scenes variables for implementing google maps
-        a_dict['lat'] = 44.92057
-        a_dict['lng'] = -93.44786
+        a_dict['lat'] = an_option.event_activity.lat
+        a_dict['lng'] = an_option.event_activity.lng
         scount = str(count)
         a_dict['id'] = "id" + scount
         a_dict['mapno'] = "map" + scount
--- ye_schedule.py
+++ ye_schedule.py
@@ -1,5 +1,7 @@
 import yelp
 import eventful
+from yelp.client import Client
+from yelp.oauth1_authenticator import Oauth1Authenticator
 
 class Location:
     def __init__(self, type, yelp_loc_obj=None, dictionary=None, address=None):
@@ -14,7 +16,12 @@ class Activity:
             self.object = object
             self.name = object.name
             self.location = Location("yelp", object.location)
-
+            self.address = object.location.address[0] + ", " + object.location.city + ", " + object.location.state_code
+            self.phone = object.display_phone[3:]
+            self.rating = object.rating
+            self.lat = object.location.coordinate.latitude
+            self.lng = object.location.coordinate.longitude
+        
         elif self.type_str == "eventful":
             #similar to above but for an eventful object
             self.object = object
@@ -22,6 +29,10 @@ class Activity:
             self.location = Location("address", dictionary=object['venue_address'])
             self.start_time = object['start_time']
             self.end_time = object['stop_time']
+            self.venue = object['venue_name'] + ", " + object['venue_address'] + ", " + object['city_name'] + ", " + object['region_name']
+            self.description = object['description']
+            self.lat = object['latitude']
+            self.lng = object['longitude']
 
     def __str__(self):
         return str(self.name) + ", " + str(self.location)
@@ -44,25 +55,36 @@ class EventAndDiningPair:
 
 
 class ScheduleMaker:
-    def __init__(self, yelp_results, eventful_results, num_events_listed, num_dining_opts_per_event):
-        self.yelp_results = yelp_results
+    def __init__(self, eventful_results, num_events_listed, num_dining_opts_per_event, yelp_params):
         self.eventful_results = eventful_results
         self.options_list = []
         self.num_events_listed = num_events_listed
         self.num_dining_opts_per_event = num_dining_opts_per_event
+        self.yelp_params = yelp_params
         self.findScheduleOptions()
-
-
+    
     def findScheduleOptions(self):
         self.options_list = []
         #Create a pair of activities for each of the combinations of one result from yelp and one from eventful
         for i in range(self.num_events_listed):
+           
             event_activity = Activity(self.eventful_results[i], "eventful")
+            
+            lat = self.eventful_results[i]['latitude']  #
+            lng = self.eventful_results[i]['longitude']  #
             dining_activities = []
+    
+            auth = Oauth1Authenticator(consumer_key = "mvC1SY7wAUx_RPlVhG-fIw",consumer_secret = "vkqWVoowxsWkUu7KU0t2Pj3qY1k",token =  "syYEIdwvGt-uLGdgVmu9ZsQfE98CROe4", token_secret = "AQLQnKJA7VYw4XlVIMK7hYzSwDo") #
+            client = Client(auth)  #
+            
+            yelp_results = client.search_by_coordinates(lat, lng, **self.yelp_params)
             for j in range(self.num_dining_opts_per_event):
-                dining = Activity(self.yelp_results.businesses[j], "yelp")
+                #print(location.businesses[j])
+                #print(self.yelp_results.businesses[j])
+                dining = Activity(yelp_results.businesses[j], "yelp")
                 dining_activities.append(dining)
             self.options_list.append(EventAndDiningPair(event_activity, dining_activities))
+
 
     #Not a priority, but important
     def orderOptions(self):
